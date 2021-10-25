4967686ea8eb9a553e5288c3ab6219e5aaa46116
felixgarcia97 <92739396+felixgarcia97@users.noreply.github.com>
2021-10-25 10:06:48 -0500

    Add files via upload

---
+++ login.js
@@ -0,0 +1,13 @@
+function validar(){
+   var us, co;
+   us=document.getElementById("usuario").value;
+   co=document.getElementById("contraseña").value;
+   if(us== "Felix" && co=="mendoza"){
+      alert("Tu usuario y contraseña son validos");
+      window.open("peso.html");
+        
+   }
+   else{
+      alert("Tu usuario y contraseña no son validos");
+   }
+}
